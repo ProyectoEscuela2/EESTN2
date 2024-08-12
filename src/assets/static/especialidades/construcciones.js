89297a532697458e89e61dee224f9f03d7a32ffc
emmassalazar <es8971521@gmail.com>
2024-08-12 00:57:55 -0300

    .

--- src/assets/static/especialidades/construcciones.js
+++ src/assets/static/especialidades/construcciones.js
@@ -2,7 +2,7 @@ export const MMO_DATA = {
     "videoURL": "https://www.youtube.com/embed/T1ZtE9weu-s",
     "titulo":"Construcciones",
     "acercaDeEspecialidad": [ // [Paragraph, Paragraph, ..., Paragraph]
-        "La especialidad de MMO dura 4 años. Nuestra escuela es la única que tiene esta especialidad en toda la ciudad. En esta especialidad hay mucho dibujo y maquetado, ya sea a mano o digitalmente con programas de diseño como AutoCAD. Es un área que sirve mucho para personas que busquen ser arquitectos o ingenieros civiles. La materia central de construcciones es Proyecto y además cuenta con materias como sistemas constructivos, planificación de obra, estructuras e instalaciones eléctricas y sanitarias.",
+        "La especialidad de Maestro Mayor de Obras dura 4 años. Nuestra escuela es la única que tiene esta especialidad en toda la ciudad. En esta especialidad hay mucho dibujo y maquetado, ya sea a mano o digitalmente con programas de diseño como AutoCAD. Es un área que sirve mucho para personas que busquen ser arquitectos o ingenieros civiles. La materia central de construcciones es Proyecto y además cuenta con materias como sistemas constructivos, planificación de obra, estructuras e instalaciones eléctricas y sanitarias.",
     ],
     "diseñoCurricularSRC": "https://drive.google.com/file/d/1KGKMWqNjreOedjlA8dXXkZmM6_2NV6-l/view",
     "practicasInfo": [
