1bb405dd799a7dde034a469d82e65151f3ca0f26
Alejito <Notebook@gmail.com>
2024-08-20 10:05:40 -0300

    Imágenes WebP, Reparación Galería (Collage)

--- src/assets/static/Galerias/imagenes.js
+++ src/assets/static/Galerias/imagenes.js
@@ -2,25 +2,25 @@ export const IMAGENES = [
     // Informática
     {
         "id": 0,
-        "url": "/imagenes/imagenes/informatica/imagen1.jpg",
+        "url": "/imagenes/imagenes/informatica/imagen1.webp",
         "alt": "Imagen",
         "especialidad": "Informática"
     },
     {
         "id": 1,
-        "url": "/imagenes/imagenes/informatica/imagen2.jpg",
+        "url": "/imagenes/imagenes/informatica/imagen2.webp",
         "alt": "Imagen",
         "especialidad": "Informática"
     },
     {
         "id": 2,
-        "url": "/imagenes/imagenes/informatica/imagen3.jpg",
+        "url": "/imagenes/imagenes/informatica/imagen3.webp",
         "alt": "Imagen",
         "especialidad": "Informática"
     },
     {
         "id": 3,
-        "url": "/imagenes/imagenes/informatica/imagen4.jpg",
+        "url": "/imagenes/imagenes/informatica/imagen4.webp",
         "alt": "Imagen",
         "especialidad": "Informática"
     },
@@ -28,62 +28,62 @@ export const IMAGENES = [
     // Electromecánica
     {
         "id": 4,
-        "url": "/imagenes/imagenes/electromecanica/elect0.jpeg",
+        "url": "/imagenes/imagenes/electromecanica/elect0.webp",
         "alt": "Imagen",
         "especialidad": "Electromecánica"
     },
     {
         "id": 5,
-        "url": "/imagenes/imagenes/electromecanica/elect1.jpeg",
+        "url": "/imagenes/imagenes/electromecanica/elect1.webp",
         "alt": "Imagen",
         "especialidad": "Electromecánica"
     },
     {
         "id": 6,
-        "url": "/imagenes/imagenes/electromecanica/elect2.jpeg",
+        "url": "/imagenes/imagenes/electromecanica/elect2.webp",
         "alt": "Imagen",
         "especialidad": "Electromecánica"
     },            
     {
         "id": 7,
-        "url": "/imagenes/imagenes/electromecanica/elect3.jpeg",
+        "url": "/imagenes/imagenes/electromecanica/elect3.webp",
         "alt": "Imagen",
         "especialidad": "Electromecánica"
     },            
     {
         "id": 8,
-        "url": "/imagenes/imagenes/electromecanica/elect4.jpeg",
+        "url": "/imagenes/imagenes/electromecanica/elect4.webp",
         "alt": "Imagen",
         "especialidad": "Electromecánica"
     },            
     {
         "id": 9,
-        "url": "/imagenes/imagenes/electromecanica/elect5.jpeg",
+        "url": "/imagenes/imagenes/electromecanica/elect5.webp",
         "alt": "Imagen",
         "especialidad": "Electromecánica"
 
     },
     {
         "id": 10,
-        "url": "/imagenes/imagenes/electromecanica/elect6.jpeg",
+        "url": "/imagenes/imagenes/electromecanica/elect6.webp",
         "alt": "Imagen",
         "especialidad": "Electromecánica"
     },
     {
         "id": 11,
-        "url": "/imagenes/imagenes/electromecanica/elect7.jpeg",
+        "url": "/imagenes/imagenes/electromecanica/elect7.webp",
         "alt": "Imagen",
         "especialidad": "Electromecánica"
     },
     {
         "id": 12,
-        "url": "/imagenes/imagenes/electromecanica/elect8.jpeg",
+        "url": "/imagenes/imagenes/electromecanica/elect8.webp",
         "alt": "Imagen",
         "especialidad": "Electromecánica"
     },
     {
         "id": 13,
-        "url": "/imagenes/imagenes/electromecanica/elect9.jpeg",
+        "url": "/imagenes/imagenes/electromecanica/elect9.webp",
         "alt": "Imagen",
         "especialidad": "Electromecánica"
     },
@@ -91,199 +91,199 @@ export const IMAGENES = [
     // Química
     {
         "id": 14,
-        "url": "/imagenes/imagenes/quimica/imagen1.jpg",
+        "url": "/imagenes/imagenes/quimica/imagen1.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },
     {
         "id": 15,
-        "url": "/imagenes/imagenes/quimica/imagen2.jpg",
+        "url": "/imagenes/imagenes/quimica/imagen2.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },
     {
         "id": 16,
-        "url": "/imagenes/imagenes/quimica/imagen3.jpg",
+        "url": "/imagenes/imagenes/quimica/imagen3.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },
     {
         "id": 17,
-        "url": "/imagenes/imagenes/quimica/imagen4.jpg",
+        "url": "/imagenes/imagenes/quimica/imagen4.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },    {
         "id": 18,
-        "url": "/imagenes/imagenes/quimica/imagen5.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen5.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },
     {
         "id": 19,
-        "url": "/imagenes/imagenes/quimica/imagen6.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen6.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },
     {
         "id": 20,
-        "url": "/imagenes/imagenes/quimica/imagen7.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen7.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },
     {
         "id": 21,
-        "url": "/imagenes/imagenes/quimica/imagen8.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen8.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },
     {
         "id": 22,
-        "url": "/imagenes/imagenes/quimica/imagen9.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen9.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 23,
-        "url": "/imagenes/imagenes/quimica/imagen10.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen10.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 24,
-        "url": "/imagenes/imagenes/quimica/imagen11.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen11.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 25,
-        "url": "/imagenes/imagenes/quimica/imagen12.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen12.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 26,
-        "url": "/imagenes/imagenes/quimica/imagen13.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen13.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 27,
-        "url": "/imagenes/imagenes/quimica/imagen14.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen14.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 28,
-        "url": "/imagenes/imagenes/quimica/imagen15.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen15.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 29,
-        "url": "/imagenes/imagenes/quimica/imagen16.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen16.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 30,
-        "url": "/imagenes/imagenes/quimica/imagen17.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen17.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 31,
-        "url": "/imagenes/imagenes/quimica/imagen18.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen18.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 32,
-        "url": "/imagenes/imagenes/quimica/imagen19.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen19.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 33,
-        "url": "/imagenes/imagenes/quimica/imagen20.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen20.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 34,
-        "url": "/imagenes/imagenes/quimica/imagen21.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen21.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 35,
-        "url": "/imagenes/imagenes/quimica/imagen22.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen22.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 36,
-        "url": "/imagenes/imagenes/quimica/imagen23.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen23.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 37,
-        "url": "/imagenes/imagenes/quimica/imagen24.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen24.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 38,
-        "url": "/imagenes/imagenes/quimica/imagen25.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen25.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 39,
-        "url": "/imagenes/imagenes/quimica/imagen26.jpeg",
+        "url": "/imagenes/imagenes/quimica/imagen26.webp",
         "alt": "Imagen",
         "especialidad": "Química"
     },{
         "id": 40,
-        "url": "/imagenes/imagenes/mmo/imagen1.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen1.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },{
         "id": 41,
-        "url": "/imagenes/imagenes/mmo/imagen2.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen2.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },{
         "id": 42,
-        "url": "/imagenes/imagenes/mmo/imagen3.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen3.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },{
         "id": 43,
-        "url": "/imagenes/imagenes/mmo/imagen4.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen4.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },{
         "id": 44,
-        "url": "/imagenes/imagenes/mmo/imagen5.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen5.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },{
         "id": 45,
-        "url": "/imagenes/imagenes/mmo/imagen6.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen6.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },{
         "id": 46,
-        "url": "/imagenes/imagenes/mmo/imagen7.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen7.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },{
         "id": 47,
-        "url": "/imagenes/imagenes/mmo/imagen8.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen8.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },{
         "id": 48,
-        "url": "/imagenes/imagenes/mmo/imagen9.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen9.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },{
         "id": 49,
-        "url": "/imagenes/imagenes/mmo/imagen10.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen10.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },{
         "id": 50,
-        "url": "/imagenes/imagenes/mmo/imagen11.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen11.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },{
         "id": 51,
-        "url": "/imagenes/imagenes/mmo/imagen12.jpg",
+        "url": "/imagenes/imagenes/mmo/imagen12.webp",
         "alt": "Imagen",
         "especialidad": "Construcciones"
     },
--- src/assets/static/especialidades/empresas.js
+++ src/assets/static/especialidades/empresas.js
@@ -4,7 +4,7 @@ export const EMP_DATA = [
      "id": 0,
      "nombre": "Ternium",
      "ubicacion": "Av. Savio 1972",   
-     "logo": "imagenes/logos/TERNIUM.jpeg",
+     "logo": "imagenes/logos/TERNIUM.webp",
      "title": "Empresa metalúrgica"
 
     },
@@ -12,14 +12,14 @@ export const EMP_DATA = [
         "id": 1,
         "nombre": "Sidersa",
         "ubicacion": "RN9 Km 226,5",   
-        "logo": "imagenes/logos/SIDERSA.jpeg",
+        "logo": "imagenes/logos/SIDERSA.webp",
         "title" : "Servicios siderometalúrgicos"
     },
     {
         "id": 2,
         "nombre": "ACA",
         "ubicacion": "Dr. Román Subiza 1240",
-        "logo": "imagenes/logos/ACA.jpeg",
+        "logo": "imagenes/logos/ACA.webp",
         "title": "Sistema cooperativo de soluciones agropecuarias"
     },
     {
@@ -40,27 +40,27 @@ export const EMP_DATA = [
         "id": 5,
         "nombre": "Municipalidad de San Nicolas",
         "ubicacion":"Pres. Illia 1130",
-        "logo": "imagenes/logos/Muni.jpeg",
+        "logo": "imagenes/logos/Muni.webp",
         "title": "Municipalidad en San Nicolás de los Arroyos"
 
     },{
         "id": 6,
         "nombre": "Moviport",
         "ubicacion":"Avenida Las Rosas 190",
-        "logo": "imagenes/logos/MoviPort.jpeg",
+        "logo": "imagenes/logos/MoviPort.webp",
         "title": "Empresa orientada a proveer soluciones logísticas"
     },{
         "id": 7,
         "nombre": "G2K",
         "ubicacion":"León Guruciaga 216",
-        "logo": "imagenes/logos/G2K.jpg",
+        "logo": "imagenes/logos/G2K.webp",
         "title":"Servicio de alojamiento web"
 
     },{
         "id": 8,
         "nombre": "ProvSer",
         "ubicacion":"Parque Industrial COMIRSA, Calle 3, N° 1090",
-        "logo": "imagenes/logos/Provser.jpeg",
+        "logo": "imagenes/logos/Provser.webp",
         "title": "Desarrollos técnicos para la cadena de valor en áreas de movimentación"
     },    
     {
@@ -76,61 +76,61 @@ export const EMP_DATA = [
         "id": 11,
         "nombre": "Loberaz",
         "ubicacion":"Parque Industrial COMIRSA, Calle 3, n° 1155",
-        "logo": "imagenes/logos/LOBERAZ.jpeg",
+        "logo": "imagenes/logos/LOBERAZ.webp",
         "title": "Construcciones y montajes industriales, livianos y pesados"
     },{
         "id": 12,
         "nombre": "ARSUC Metal",
         "ubicacion":"Central Acero Argentino Este 698",
-        "logo": "imagenes/logos/ARSUC.jpeg",
+        "logo": "imagenes/logos/ARSUC.webp",
         "title": "Taller de trabajo de metales"
     },{
         "id": 13,
         "nombre": "AGUAS",
         "ubicacion":"Av. Illia 1130",
-        "logo": "imagenes/logos/AGUAS.jpeg",
+        "logo": "imagenes/logos/AGUAS.webp",
         "title": "Organismo descentralizado de la administración pública municipal"
     },{
         "id": 14,
         "nombre": "ZETATEC",
         "ubicacion":"Parque Industrial COMIRSA, Calle 3 Este 1065 bis",
-        "logo": "imagenes/logos/ZETATEC.jpeg",
+        "logo": "imagenes/logos/ZETATEC.webp",
         "title": "Empresa de construcción"
     },{
         "id": 15,
         "nombre": "Transba",
         "ubicacion":"San José 647",
-        "logo": "imagenes/logos/TRANSBA.jpeg",
+        "logo": "imagenes/logos/TRANSBA.webp",
         "title": "Compañía de servicios públicos de electricidad"
     },{
         "id": 16,
         "nombre": "San Sebastian",
         "ubicacion":"Parque Industrial COMIRSA, Calle 3",
-        "logo": "imagenes/logos/SS.jpeg",
+        "logo": "imagenes/logos/SS.webp",
         "title": "Empresa de transporte"
     },{
         "id": 17,
         "nombre": "Sabecort",
         "ubicacion":"Av. Moreno 18",
-        "logo": "imagenes/logos/SABECORT.jpeg",
+        "logo": "imagenes/logos/SABECORT.webp",
         "title": "Empresa dedicada a la venta y colocación de equipos de GNC"
     },{
         "id": 18,
         "nombre": "Puerto San Nicolás",
         "ubicacion":"Avenida Román Subiza y Ruta Nacional 188",
-        "logo": "imagenes/logos/Puerto.jpeg",
+        "logo": "imagenes/logos/Puerto.webp",
         "title": "Actividad portuaria"
     },{
         "id": 19,
         "nombre": "PTP Group",
         "ubicacion":"Avenida Román Subiza y Ruta Nacional 188(Actividad por subcontratacion)",
-        "logo": "imagenes/logos/PTP.jpeg",
+        "logo": "imagenes/logos/PTP.webp",
         "title": "Operaciones portuarias, transporte y cargas"
     },{
         "id": 20,
         "nombre": "Pamsa",
         "ubicacion":"Dr. Román Subiza 1750",
-        "logo": "imagenes/logos/pamsa.jpeg",
+        "logo": "imagenes/logos/pamsa.webp",
         "title": "Planta química"
     },{
         "id": 21,
