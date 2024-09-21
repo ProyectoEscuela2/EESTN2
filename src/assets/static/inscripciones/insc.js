9b941b8b473e02cf94d633fb840ab50e5535ae07
ramirovega193 <cuentacarreras193@gmail.com>
2024-09-21 01:39:51 -0300

    Archivos inscripcion de primero

--- src/assets/static/inscripciones/insc.js
+++ src/assets/static/inscripciones/insc.js
@@ -1,10 +1,81 @@
-export const INSCRIPCION_DATA = [
+export const INSCRIPCION_HNOS = [
     
+    {
+        "id":"0",
+        "title":"Planilla de inscripcion",
+        "text":"Descargar Archivo",
+        "urlDescarga":"/archivos/hermanos/1. PLANILLA DE INSCRIPCIÓN.pdf",
+    },
+    {
+        "id":"1",
+        "title":"Acta compromiso",
+        "text":"Descargar Archivo",
+        "urlDescarga":"/archivos/hermanos/2. ACTA COMPROMISO DEL ESTABLECIMIENTO 1 AÑO 2025.pdf"
+    },    
+    {
+        "id":"2",
+        "title":"Anexo vestimenta",
+        "text":"Descargar Archivo",
+        "urlDescarga":"/archivos/hermanos/3. ANEXO VESTIMENTA 2025.pdf"
+    },
+    {
+        "id":"3",
+        "title":"Ficha datos ciclo basico",
+        "text":"Descargar Archivo",
+        "urlDescarga":"/archivos/hermanos/4. FICHA DATOS CICLO BÁSICO 2025.pdf"
+    },
+    {
+        "id":"4",
+        "title":"Autorizaciones salidas y uso de imagen",
+        "text":"Descargar Archivo",
+        "urlDescarga":"/archivos/hermanos/5. AUTORIZACIÓN SALIDA- ACTA USO DE IMAGEN 2025.pdf"
+    },
+    {
+        "id":"5",
+        "title":"Acuerdo institucional de convivencia",
+        "text":"Descargar Archivo",
+        "urlDescarga":"/archivos/hermanos/6. ACUERDO INSTITUCIONAL DE CONVIVENCIA.pdf"
+    },
+    {
+        "id":"6",
+        "title":"Cuadernillo matematica",
+        "text":"Descargar Archivo",
+        "urlDescarga":"/archivos/hermanos/Cuadernillo_Matemática_1°año_2025.pdf"
+    },{
+        "id":"7",
+        "title":"Cuadernillo Ingles",
+        "text":"Descargar Archivo",
+        "urlDescarga":"/archivos/hermanos/Cuadernillo_English Booklet_1°año_2025.pdf"
+    },{
+        "id":"8",
+        "title":"Cuadernillo Practicas del lenguaje",
+        "text":"Descargar Archivo",
+        "urlDescarga":"/archivos/hermanos/Cuadernillo_Prácticas de Lenguaje_1° año_2025.pdf"
+    },
+]
+
+export const INSCRIPCION_PRIMERO = [
     {
         "id":"0",
         "title":"Requisito 1",
         "text":"Descargar Archivo",
+        "urlDescarga":"/temporary/fake/route.pdf",
+    },
+    {
+        "id":"1",
+        "title":"Requisito 2",
+        "text":"Descargar Archivo",
         "urlDescarga":"/temporary/fake/route.pdf"
+    },
+]
+
+
+export const INSCRIPCION_GRAL = [
+    {
+        "id":"0",
+        "title":"Requisito 1",
+        "text":"Descargar Archivo",
+        "urlDescarga":"/temporary/fake/route.pdf",
     },
     {
         "id":"1",
--- src/pages/inscribirse.jsx
+++ src/pages/inscribirse.jsx
@@ -1,27 +1,31 @@
-import { INSCRIPCION_DATA } from "../assets/static/inscripciones/insc"
+import { INSCRIPCION_HNOS } from "../assets/static/inscripciones/insc"
 import "../styles/inscribirse.css"
 export default function Inscribirse(){
 
 
+    console.log()
     //Para agregar archivos hay que modificar el archivo en static/inscripciones/insc.js
     return<>
     
 
     <section className="inscribirse">
     <h2>Inscripciones</h2>
-    <h3>Las inscripciones están cerradas por ahora, ¡volvé en otro momento!</h3>
 
-    {/* <h3>Requisitos</h3>
+    <h3>Inscripción para hermanos de alumnos</h3>
     <div className="inscribirse-requisitos">
-        {INSCRIPCION_DATA.map((data) => (
-
+   
+        {INSCRIPCION_HNOS.map((data) => (
             <div key={data.id} className="inscribirse-contenedor">
                 <h4>{data.title}</h4>
                 <a href={data.urlDescarga}><button>{data.text}</button></a>
             </div>
 
         ))}
-    </div> */}
+    </div>
+
+    <h3>El restos de las inscripciones abrira pronto.</h3>
+
+
     </section>
     </>
 
